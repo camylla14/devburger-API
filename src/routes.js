810437b34919f91d4ed77b118e7d31b1198de268
camylla14 <vcamylla222@gmail.com>
2025-07-02 22:50:41 -0300

    Configuração do Model de Usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-	return response.status(200).json({ message: 'Hello World' });
+routes.get('/', async (request, response) => {
+	const user = await User.create({
+		id: v4(),
+		name: 'Camylla2',
+		email: 'camylla2@gmail.com',
+		password_hash: 'hdjsndsndi',
+	});
+
+	return response.status(201).json(user);
 });
 
 export default routes;
