47a93ec7539baf884d9b3d58a0ca57e48d92634b
camylla14 <vcamylla222@gmail.com>
2025-08-08 11:49:37 -0300

    altera o multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,12 +1,14 @@
 import multer from 'multer';
+
 import { v4 } from 'uuid';
 
 import { extname, resolve } from 'node:path';
 
 export default {
-	Storage: multer.diskStorage({
+	storage: multer.diskStorage({
 		destination: resolve(__dirname, '..', '..', 'uploads'),
-		filename: (request, File, callback) =>
-			callback(null, v4() + extname(File.originalname)),
+
+		filename: (request, file, callback) =>
+			callback(null, v4() + extname(file.originalname)),
 	}),
 };
