80cdcfde983e9bb62b93e9fa7fdf7d6faaf073e2
camylla14 <vcamylla222@gmail.com>
2025-08-12 22:04:12 -0300

    Criando Model de Produtos

--- src/routes.js
+++ src/routes.js
@@ -17,5 +17,6 @@ routes.post('/users', (req, res) => {
 routes.post('/session', SessionController.store);
 
 routes.post('/products', upload.single('file'), ProductController.store);
+routes.get('/products', ProductController.index);
 
 export default routes;
