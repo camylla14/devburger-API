9c682e55c3330b5daec0a973f157e79ea187d146
camylla14 <vcamylla222@gmail.com>
2025-08-14 12:34:15 -0300

    Configurando token JWT pt1

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
 import User from '../models/User';
-
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+import auth from '../../config/auth';
 class SessionController {
 	async store(request, response) {
 		const schema = Yup.object({
@@ -43,6 +45,9 @@ class SessionController {
 			name: user.name,
 			email,
 			admin: user.admin,
+			token: jwt.sign({ id: user.id }, authConfig.secret, {
+				expiresIn: authConfig.expiresIn,
+			}),
 		});
 	}
 }
